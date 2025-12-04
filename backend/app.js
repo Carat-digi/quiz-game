77bf0dc2e455c60727bb1f6bdf4db78a092aba5a
Sofia <thesavchukova@gmail.com>
2025-12-04 02:39:05 +0200

    configure CORS and routes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const config = require('./src/utils/config')
 const logger = require('./src/utils/logger')
-const authRouter = require('./src/routes/auth')
+const authRouter = require('./src/routes/authRout')
+const quizRouter = require('./src/routes/quizRout')
+const AppError = require('./src/utils/appError')
+const errorHandler = require('./src/middleware/errorHandler')
 
 const app = express()
 
@@ -16,14 +21,27 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB:', err.message)
   })
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
+
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(cookieParser())
 
 // API routes
 app.use('/api/auth', authRouter)
+app.use('/api/quizz', quizRouter)
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to the Backend!</h1>')
 })
+
+app.all(/.*/, (req, res, next) => {
+  next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(errorHandler)
 
 module.exports = app
