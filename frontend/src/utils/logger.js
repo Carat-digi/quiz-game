0fea2b39f0b11e1fd48ae8093ab1a3ee09d3f4e7
Sofia <thesavchukova@gmail.com>
2025-12-12 10:15:04 +0200

    add logger utility in frontend

---
+++ frontend/src/utils/logger.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+const isDevelopment = import.meta.env.DEV
+
+const logger = {
+  log: (...args) => {
+    if (isDevelopment) {
+      console.log(...args)
+    }
+  },
+  error: (...args) => {
+    if (isDevelopment) {
+      console.error(...args)
+    }
+  },
+  warn: (...args) => {
+    if (isDevelopment) {
+      console.warn(...args)
+    }
+  },
+  info: (...args) => {
+    if (isDevelopment) {
+      console.info(...args)
+    }
+  }
+}
+
+export default logger
